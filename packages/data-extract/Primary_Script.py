3b2ff3bbc8073d78f7f002f98543cf735a6a81ed
sumukshashidhar <sumukuuu@gmail.com>
2020-04-11 17:06:24 +0530

    Fixed a bug

--- packages/data-extract/Primary_Script.py
+++ packages/data-extract/Primary_Script.py
@@ -18,7 +18,7 @@ def get_csv(command):
 
 
 def filename():
-	name = date.today() + '.csv'
+	name = str(date.today()) + '.csv'
 	return name
 
 cmd = 'curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://www.nseindia.com/api/allIndices?csv=true -o ' + filename()
