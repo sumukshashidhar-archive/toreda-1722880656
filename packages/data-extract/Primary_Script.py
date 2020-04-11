129f8b8823708611245877cbb032b7b416f85780
sumukshashidhar <sumukuuu@gmail.com>
2020-04-11 17:15:23 +0530

    Added a path function

--- packages/data-extract/Primary_Script.py
+++ packages/data-extract/Primary_Script.py
@@ -12,13 +12,15 @@ import time
 import schedule
 
 
+path = os.path.realpath('..')[:-8] #path of the entire folder
+
 def get_csv(command):
 	os.system(command)
 
 
 
 def filename():
-	name = str(date.today()) + '.csv'
+	name = path + str(date.today()) + '.csv'
 	return name
 
 cmd = 'curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://www.nseindia.com/api/allIndices?csv=true -o ' + filename()
