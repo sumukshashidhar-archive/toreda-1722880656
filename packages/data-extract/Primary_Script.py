58c76979cfd830b24529c045b8ec80059fb4da06
sumukshashidhar <sumukuuu@gmail.com>
2020-04-11 17:26:27 +0530

    Finished the scheduling job

--- packages/data-extract/Primary_Script.py
+++ packages/data-extract/Primary_Script.py
@@ -11,7 +11,6 @@ from datetime import date
 import time
 import schedule
 
-
 path = os.path.realpath('..')[:-8] + 'data' + '/indices' #path of the dataset_folder
 
 def get_csv(command):
@@ -23,5 +22,17 @@ def filename():
 	name = path + str(date.today()) + '.csv'
 	return name
 
-cmd = 'curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://www.nseindia.com/api/allIndices?csv=true -o ' + filename()
-get_csv(cmd)
+
+## defining some constants
+cmd = 'curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://www.nseindia.com/api/allIndices?csv=true -o '
+
+def get():
+	com = cmd + filename()
+	get_csv(com)
+
+schedule.every.day().at("21:00").do(get_csv)
+
+
+while True:
+	schedule.run_pending()
+	time.sleep(1000)
