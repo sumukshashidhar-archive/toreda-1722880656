cccd590cac1ecc08e3923da2da538b272a1a419d
sumukshashidhar <sumukuuu@gmail.com>
2020-04-11 17:36:26 +0530

    Finished the schedule job

--- packages/data-extract/Primary_Script.py
+++ packages/data-extract/Primary_Script.py
@@ -12,10 +12,15 @@ import time
 import schedule
 
 path = os.path.realpath('..')[:-8] + 'data' + '/indices' #path of the dataset_folder
+push_cmd_1 = 'git add' + path + ' && git commit -a -m "File Commit: '
+push_cmd_2 = '" && git push'
 
 def get_csv(command):
 	os.system(command)
 
+
+def push(pushcmd):
+	os.system(pushcmd)
 
 
 def filename():
@@ -27,10 +32,14 @@ def filename():
 cmd = 'curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" https://www.nseindia.com/api/allIndices?csv=true -o '
 
 def get():
-	com = cmd + filename()
+	n = filename()
+	com = cmd + n
 	get_csv(com)
+	time.sleep(100)
+	pushcmd = push_cmd + n + push_cmd_2
+	push(pushcmd)
 
-schedule.every.day().at("21:00").do(get_csv)
+schedule.every.day().at("21:00").do(get)
 
 
 while True:
