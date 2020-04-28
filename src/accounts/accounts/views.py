c341704d5c3b67d61522b1eb69c09cdc52079e62
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-04-28 11:30:23 +0530

    Made changes

--- src/accounts/accounts/views.py
+++ src/accounts/accounts/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
