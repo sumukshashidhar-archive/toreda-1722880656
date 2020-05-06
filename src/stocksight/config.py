d004c46aea1f7eab8987b977bac5bd3ffbaf6a83
sumukshashidhar <sumukuuu@gmail.com>
2020-05-06 20:50:08 +0530

    Added StockSight

---
+++ src/stocksight/config.py
@@ -0,0 +1,22 @@
+stocksight_token = "c53e9d96ab61cc247dd2e7e7431dbe0d5096bf4776d7895e7a82142411e49c709ebd3d039840a3c3fd09aa7215d554c26f62"
+elasticsearch_host = "elasticsearch"
+elasticsearch_port = 9200
+elasticsearch_user = ""
+elasticsearch_password = ""
+consumer_key = "FUPWR8oiyCRa7RwLY5mqSQdR0"
+consumer_secret = "U7qijEyqCegOr4gdwRW03X9ypTP72biZgFLJW1qzyXiVitQEQr"
+access_token = "762166371765456897-6lLHVr7GyrvYYCxGDWIelZFLThvnoqj"
+access_token_secret = "RPwSGoJNDJh7fsCKlMum1ErMoLmLUj9jHkDBRKciYcBsS"
+nltk_tokens_required = ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
+stocksight_token = "c53e9d96ab61cc247dd2e7e7431dbe0d5096bf4776d7895e7a82142411e49c709ebd3d039840a3c3fd09aa7215d554c26f62"
