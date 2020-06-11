754d258937d24b412c58cdaa3d1e65c09c1b9d26
sumukshashidhar <sumukuuu@gmail.com>
2020-06-11 19:43:53 +0530

    can now pull data from alphavantage

--- src/high-frequency-trading/datastream.py
+++ src/high-frequency-trading/datastream.py
@@ -1,1 +1,13 @@
-## this will stream data using alphavantage
+#this will stream data using alphavantage
+
+import pandas as pd
+
+SYMBOL = 'NSE:INFY'
+INTERVAL = '5min'
+API_KEY = input()
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={SYMBOL}&interval={INTERVAL}&apikey={API_KEY}&datatype=csv'
+
+
+df = pd.read_csv(url)
+
+print(df.head)
