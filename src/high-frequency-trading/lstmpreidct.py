49e6ca81b37e9f0cabd02e91ecf0526fa25369f3
sumukshashidhar <sumukuuu@gmail.com>
2020-06-12 21:39:29 +0530

    finished the LSTM module

---
+++ src/high-frequency-trading/lstmpreidct.py
@@ -0,0 +1,256 @@
+## Imports
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from yahoo_fin import stock_info as si
+from collections import deque
+
+from datetime import date
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import os
+import random
+
+def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1,
+                test_size=0.2, feature_columns=['adjclose', 'volume', 'open', 'high', 'low']):
+    # see if ticker is already a loaded stock from yahoo finance
+    if isinstance(ticker, str):
+        # load it from yahoo_fin library
+        df = si.get_data(ticker)
+    elif isinstance(ticker, pd.DataFrame):
+        # already loaded, use it directly
+        df = ticker
+    # this will contain all the elements we want to return from this function
+    result = {}
+    # we will also return the original dataframe itself
+    result['df'] = df.copy()
+    # make sure that the passed feature_columns exist in the dataframe
+    for col in feature_columns:
+        assert col in df.columns, f"'{col}' does not exist in the dataframe."
+    if scale:
+        column_scaler = {}
+        # scale the data (prices) from 0 to 1
+        for column in feature_columns:
+            scaler = preprocessing.MinMaxScaler()
+            df[column] = scaler.fit_transform(np.expand_dims(df[column].values, axis=1))
+            column_scaler[column] = scaler
+
+        # add the MinMaxScaler instances to the result returned
+        result["column_scaler"] = column_scaler
+    # add the target column (label) by shifting by `lookup_step`
+    df['future'] = df['adjclose'].shift(-lookup_step)
+    # last `lookup_step` columns contains NaN in future column
+    # get them before droping NaNs
+    last_sequence = np.array(df[feature_columns].tail(lookup_step))
+    # drop NaNs
+    df.dropna(inplace=True)
+    sequence_data = []
+    sequences = deque(maxlen=n_steps)
+    for entry, target in zip(df[feature_columns].values, df['future'].values):
+        sequences.append(entry)
+        if len(sequences) == n_steps:
+            sequence_data.append([np.array(sequences), target])
+    # get the last sequence by appending the last `n_step` sequence with `lookup_step` sequence
+    # for instance, if n_steps=50 and lookup_step=10, last_sequence should be of 59 (that is 50+10-1) length
+    # this last_sequence will be used to predict in future dates that are not available in the dataset
+    last_sequence = list(sequences) + list(last_sequence)
+    # shift the last sequence by -1
+    last_sequence = np.array(pd.DataFrame(last_sequence).shift(-1).dropna())
+    # add to result
+    result['last_sequence'] = last_sequence
+    # construct the X's and y's
+    X, y = [], []
+    for seq, target in sequence_data:
+        X.append(seq)
+        y.append(target)
+    # convert to numpy arrays
+    X = np.array(X)
+    y = np.array(y)
+    # reshape X to fit the neural network
+    X = X.reshape((X.shape[0], X.shape[2], X.shape[1]))
+    # split the dataset
+    result["X_train"], result["X_test"], result["y_train"], result["y_test"] = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    # return the result
+    return result
+
+
+def create_model(sequence_length, units=256, cell=LSTM, n_layers=2, dropout=0.3,
+                loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False):
+    model = Sequential()
+    for i in range(n_layers):
+        if i == 0:
+            # first layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True), input_shape=(None, sequence_length)))
+            else:
+                model.add(cell(units, return_sequences=True, input_shape=(None, sequence_length)))
+        elif i == n_layers - 1:
+            # last layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=False)))
+            else:
+                model.add(cell(units, return_sequences=False))
+        else:
+            # hidden layers
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True)))
+            else:
+                model.add(cell(units, return_sequences=True))
+        # add dropout after each layer
+        model.add(Dropout(dropout))
+    model.add(Dense(1, activation="linear"))
+    model.compile(loss=loss, metrics=["mean_absolute_error"], optimizer=optimizer)
+    return model
+
+def predict(model, data, N_STEPS):
+    # retrieve the last sequence from data
+    last_sequence = data["last_sequence"][:N_STEPS]
+    # retrieve the column scalers
+    column_scaler = data["column_scaler"]
+    # reshape the last sequence
+    last_sequence = last_sequence.reshape((last_sequence.shape[1], last_sequence.shape[0]))
+    # expand dimension
+    last_sequence = np.expand_dims(last_sequence, axis=0)
+    # get the prediction (scaled from 0 to 1)
+    prediction = model.predict(last_sequence)
+    # get the price (by inverting the scaling)
+    predicted_price = column_scaler["adjclose"].inverse_transform(prediction)[0][0]
+    return predicted_price
+
+
+def plot_graph(model, data, TICKER, LOOKUP_STEP, EPOCH):
+    y_test = data["y_test"]
+    X_test = data["X_test"]
+    y_pred = model.predict(X_test)
+    y_test = np.squeeze(data["column_scaler"]["adjclose"].inverse_transform(np.expand_dims(y_test, axis=0)))
+    y_pred = np.squeeze(data["column_scaler"]["adjclose"].inverse_transform(y_pred))
+
+    fig = px.line()
+    fig.add_scatter(y=y_test, name='Actual Close Values')
+    fig.add_scatter(y=y_pred, name='Predicted Close Values')
+    fname = f'./../../Generated_Graphs/{TICKER}-{LOOKUP_STEP}-{EPOCH}-{str(date.today())}.html'
+    with open(fname, 'w+') as f:
+        fig.write_html(file=f)
+    return fname
+
+def get_accuracy(model, data, LOOKUP_STEP):
+    y_test = data["y_test"]
+    X_test = data["X_test"]
+    y_pred = model.predict(X_test)
+    y_test = np.squeeze(data["column_scaler"]["adjclose"].inverse_transform(np.expand_dims(y_test, axis=0)))
+    y_pred = np.squeeze(data["column_scaler"]["adjclose"].inverse_transform(y_pred))
+    y_pred = list(map(lambda current, future: int(float(future) > float(current)), y_test[:-LOOKUP_STEP], y_pred[LOOKUP_STEP:]))
+    y_test = list(map(lambda current, future: int(float(future) > float(current)), y_test[:-LOOKUP_STEP], y_test[LOOKUP_STEP:]))
+    return accuracy_score(y_test, y_pred)
+
+
+
+
+def get_prediction(TICKER_PASS, LOOKUP_PASS, EPOCH_PASS):
+    # Window size or the sequence length
+    N_STEPS = 100
+    # Lookup step, 1 is the next day
+    LOOKUP_STEP = LOOKUP_PASS
+    # test ratio size, 0.2 is 20%
+    TEST_SIZE = 0.2
+    # features to use
+    FEATURE_COLUMNS = ["adjclose", "volume", "open", "high", "low"]
+    # date now
+    date_now = time.strftime("%Y-%m-%d")
+    ### model parameters
+    N_LAYERS = 3
+    # LSTM cell
+    CELL = LSTM
+    # 256 LSTM neurons
+    UNITS = 256
+    # 40% dropout
+    DROPOUT = 0.4
+    # whether to use bidirectional RNNs
+    BIDIRECTIONAL = False
+    ### training parameters
+    # mean absolute error loss
+    # LOSS = "mae"
+    # huber loss
+    LOSS = "huber_loss"
+    OPTIMIZER = "adam"
+    BATCH_SIZE = 64
+    EPOCHS = EPOCH_PASS
+    # Apple stock market
+    ticker = TICKER_PASS
+    ticker_data_filename = os.path.join("data", f"{ticker}_{date_now}.csv")
+    # model name to save, making it as unique as possible based on parameters
+    model_name = f"{date_now}_{ticker}-{LOSS}-{OPTIMIZER}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEP}-layers-{N_LAYERS}-units-{UNITS}"
+    if BIDIRECTIONAL:
+        model_name += "-b"
+
+    if not os.path.isdir("results"):
+        os.mkdir("results")
+    if not os.path.isdir("logs"):
+        os.mkdir("logs")
+    if not os.path.isdir("data"):
+        os.mkdir("data")
+
+    # load the data
+    data = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE, feature_columns=FEATURE_COLUMNS)
+
+    # save the dataframe
+    data["df"].to_csv(ticker_data_filename)
+
+    # construct the model
+    model = create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS,
+                         dropout=DROPOUT, optimizer=OPTIMIZER, bidirectional=BIDIRECTIONAL)
+
+    # some tensorflow callbacks
+    checkpointer = ModelCheckpoint(os.path.join("results", model_name + ".h5"), save_weights_only=True,
+                                   save_best_only=True, verbose=1)
+    tensorboard = TensorBoard(log_dir=os.path.join("logs", model_name))
+
+    history = model.fit(data["X_train"], data["y_train"],
+                        batch_size=BATCH_SIZE,
+                        epochs=EPOCHS,
+                        validation_data=(data["X_test"], data["y_test"]),
+                        callbacks=[checkpointer, tensorboard],
+                        verbose=1)
+
+    model.save(os.path.join("results", model_name) + ".h5")
+
+    data = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE,
+                     feature_columns=FEATURE_COLUMNS, shuffle=False)
+
+    # construct the model
+    model = create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS,
+                         dropout=DROPOUT, optimizer=OPTIMIZER, bidirectional=BIDIRECTIONAL)
+
+    model_path = os.path.join("results", model_name) + ".h5"
+    model.load_weights(model_path)
+
+    mse, mae = model.evaluate(data["X_test"], data["y_test"], verbose=0)
+    # calculate the mean absolute error (inverse scaling)
+    mean_absolute_error = data["column_scaler"]["adjclose"].inverse_transform([[mae]])[0][0]
+    print("Mean Absolute Error:", mean_absolute_error)
+
+    future_price = predict(model, data, N_STEPS)
+    ftstatement = f"Future price after {LOOKUP_STEP} days is ${future_price:.2f}"
+    print(ftstatement)
+
+    filepath = plot_graph(model, data, ticker, LOOKUP_STEP, EPOCHS)
+
+    acscore = get_accuracy(model, data, LOOKUP_STEP)
+
+    # %%
+
+    print(str(LOOKUP_STEP) + ":", "Accuracy Score:", acscore)
+
+    return acscore, future_price, filepath
+
+if __name__ == "__main__":
+    print(get_prediction("TSLA", 20, 10))
+
