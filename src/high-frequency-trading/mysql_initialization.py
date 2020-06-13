a80a3e2e5ae3dae73f49c70e7d82b498e2dba51d
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-13 10:39:54 +0530

    Requests table creation setup done

--- src/high-frequency-trading/mysql_initialization.py
+++ src/high-frequency-trading/mysql_initialization.py
@@ -4,6 +4,16 @@ This file is for adding all the initializations for mysql
 useful incase we need to remake the tables on a new machine
 
 """
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="mysql123rootpswd",
+  database="testdb"
+)
+
+mycursor = mydb.cursor()
 
 
 def make_request_table():
@@ -11,12 +21,15 @@ def make_request_table():
     Makes a requests table
     Must contain the stock it is requesting, the date it is requesting it on, accuracy of prediction it wants and number of days after that it wants the predicted price
     """
+    mycursor.execute("CREATE TABLE requests (id INT AUTO_INCREMENT, uuid varchar(36) PRIMARY KEY, lookupstep int, ticker VARCHAR(255), reqdate date, epochs int)")
+    
     pass
 
 def make_response_table():
     """
     Makes the response table that moves the requests once they are served
     """
+    
     pass
 
 
