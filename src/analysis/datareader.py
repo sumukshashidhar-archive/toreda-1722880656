085949d6758bf516b861973a6c8b4705616e9430
sumukshashidhar <sumukuuu@gmail.com>
2020-06-24 23:10:53 +0530

    making the keydb'

--- src/analysis/datareader.py
+++ src/analysis/datareader.py
@@ -4,31 +4,52 @@ import logging
 logging.basicConfig(level=logging.DEBUG, filename='./logs/general.log')
 logger = logging.getLogger()
 
-## Constants
+"""
+Constants Declaration
+"""
 KEYPATH = 'keys.pem'
 
+"""
+API - Key Reading and Writing Functions
+"""
+
+
 def read_keys():
-    keylist
+    """
+    Reads the keys from the specified KEYPATH and returns a list of keys as strings
+    """
+    keylist = []
     try:
         with open(KEYPATH, 'r') as f:
             keylist = f.readlines()
     except FileNotFoundError:
-        logger.CRITICAL()
+        logger.critical("Did not find the keyfile")
     keylist = [x.rstrip('\n') for x in keylist]
-    logger.DEBUG()
+    logger.debug("Created the keylist.")
     return keylist
 
+
 def create_keylogs(keylist):
+    """
+    Initializes a logging attempt for the alpha_vantage
+    """
     d = {}
     for i in keylist:
         d[i] = None
     return d
 
+def select_one_key(keydb):
+    
 
 
 def interday(self, ticker):
-    url = f''
+    """
+    Returns interday data
+    """
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={SYMBOL}&interval={INTERVAL}&apikey={API_KEY}&datatype=csv'
     pass
+
+
 
 
 
--- src/analysis/datareader_test.py
+++ src/analysis/datareader_test.py
@@ -1,18 +1,14 @@
 import unittest
+import analysis.datareader as datareader
 
-import datareader
 
 class Test(unittest.TestCase):
-
     def test_read_keys(self):
         x = datareader.read_keys()
         self.assertEqual(type(x), type([]), "Did not return a list")
         self.assertNotEqual(len(x), 0, "list does not have anything")
 
-
     def test_create_keylogs(self):
         x = datareader.create_keylogs(datareader.read_keys())
         self.assertEqual(type(x), type({}), "Did not return a dictionary")
         self.assertNotEqual(len(x.keys), 0, "dictionary does not have anything")
-
-
