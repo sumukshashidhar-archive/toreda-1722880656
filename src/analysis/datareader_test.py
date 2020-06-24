b1d8cdde5d3f8b7fa0fa239702180a6c614c7662
sumukshashidhar <sumukuuu@gmail.com>
2020-06-24 14:59:20 +0530

    adding testing modules

--- src/analysis/datareader_test.py
+++ src/analysis/datareader_test.py
@@ -1,3 +1,14 @@
-import datareader
+import unittest
+
+from datareader import testingtest
+
+class Test(unittest.TestCase):
+
+    def test_testingtest(self):
+        # testing the test module
+
+        self.assertEqual(testingtest(), True)
+        self.assertEqual(testingtest(), False)
+
 
 
