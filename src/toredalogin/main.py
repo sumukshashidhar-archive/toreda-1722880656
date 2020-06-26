2377e7ae0228a169a5e053d2b4e407b68a6100ec
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 00:58:02 +0530

    Login done

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -24,19 +24,17 @@ mysql = MySQL(app)
 def login():
     # Output message if something goes wrong...
     msg = ''
-        # Check if "username" and "password" POST requests exist (user submitted form)
+    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
-
-               # If account exists in accounts table in out database
+        # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
@@ -46,7 +44,8 @@ def login():
             return 'Logged in successfully!'
         else:
             # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!' 
-    return render_template('index.html', msg='')
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
 
 
