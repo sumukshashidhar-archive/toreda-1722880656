d88c2d290a9549378736188ac84459a750085763
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 00:41:19 +0530

    Login function and rendering template

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -17,3 +17,13 @@ app.config['MYSQL_DB'] = 'toredalogin'
 
 # Intialize MySQL
 mysql = MySQL(app)
+
+
+# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = 'Output message if something goes wrong'
+    return render_template('index.html', msg)
+
+
