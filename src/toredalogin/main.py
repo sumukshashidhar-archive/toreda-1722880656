f2094cbf364fa4957454bee2b4bfb6e4de0162ab
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 01:27:37 +0530

    Profile page route created

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -104,3 +104,18 @@ def home():
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
+
+# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
