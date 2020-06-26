65e76e3704f66c94a51d0f733fb8283f37778084
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 01:14:26 +0530

    Home page route function for logged in users

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -94,3 +94,13 @@ def register():
     return render_template('register.html', msg=msg)
 
 
+# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
+@app.route('/pythonlogin/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
