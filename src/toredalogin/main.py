a2a011fb47fd73d060739f6820293b314d1b1c93
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 00:54:50 +0530

    Checking with mysql DB for account existence and creation of session variables

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -23,12 +23,30 @@ mysql = MySQL(app)
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
-    msg = 'Output message if something goes wrong'
+    msg = ''
         # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-    return render_template('index.html', msg='Output message if something goes wrong')
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+
+               # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!' 
+    return render_template('index.html', msg='')
 
 
