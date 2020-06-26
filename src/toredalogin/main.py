d4bd672229dd8ef335274d64a2ab75e4c2f3d125
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 01:01:06 +0530

    Logout redirects user to initial login page

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -48,4 +48,14 @@ def login():
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
+# http://localhost:5000/python/logout - this will be the logout page
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
 
