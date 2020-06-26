dea70efafffcefc59eddcdd30357129ef4caf9eb
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-06-27 01:08:13 +0530

    Registration form added

--- src/toredalogin/main.py
+++ src/toredalogin/main.py
@@ -58,4 +58,21 @@ def logout():
    # Redirect to login page
    return redirect(url_for('login'))
 
+# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
 
